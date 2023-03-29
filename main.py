dc807a35cc0bb67529df594bd23e1e984846a30f
David Miles <davidmiles1925@gmail.com>
2023-03-28 23:32:26 -0500

    refactor: function definitions

--- main.py
+++ main.py
@@ -1,58 +1,74 @@
 import os
 import constants
 
-# --- Input and Validation ---
-is_int = False
-is_number = False
 
-print("Income and withholdings for 2023 in Kansas")
-print('')
-print('1) Single  2) Married  3) Head of Household')
-status_input = input('Enter your filing Status: ')
+def getGrossIncome():
+    income = input('Enter your gross income: $')
 
-while is_int == False:
-    try:
-        status_input = int(status_input)
-        if status_input < 1 or status_input > 3:
+    is_number = False
+    while is_number == False:
+        try:
+            income = float(income)
+            is_number = True
+        except:
+            print('')
+            print('Invalid input, please enter a number')
+            income = input('Enter your gross income: $')
+    return income
+
+
+def getFilingStatus():
+    is_int = False
+    status_input = input('Enter your filing Status: ')
+
+    while is_int == False:
+        try:
+            status_input = int(status_input)
+            if status_input < 1 or status_input > 3:
+                print('')
+                print('Invalid input, enter a number 1-3')
+                status_input = input('Enter your filing Status: ')
+            else:
+                return True
+        except:
             print('')
             print('Invalid input, enter a number 1-3')
             status_input = input('Enter your filing Status: ')
-        else:
-            is_int = True
-            continue
-    except:
-        print('')
-        print('Invalid input, enter a number 1-3')
-        status_input = input('Enter your filing Status: ')
+    return status_input
 
-income = input('Enter your gross income: $')
 
-while is_number == False:
-    try:
-        income = float(income)
-        is_number = True
-    except:
-        print('')
-        print('Invalid input, please enter a number')
-        income = input('Enter your gross income: $')
+def displayWelcome():
+    print("Income and withholdings for 2023 in Kansas")
+    print('')
+    print('1) Single  2) Married  3) Head of Household')
 
-# --- Status Logic --- #
-if (status_input == 1):
-    fed_cap = constants.fed_single_caps
-    kansas_cap = constants.kansas_single_caps
-    filing_status = 'Single'
-elif (status_input == 2):
-    fed_cap = constants.fed_married_caps
-    kansas_cap = constants.kansas_married_caps
-    filing_status = 'Married'
-elif (status_input == 3):
-    fed_cap = constants.fed_hoh_caps
-    kansas_cap = constants.kansas_hoh_caps
-    filing_status = 'Head of Household'
+
+def main():
+    print("test")
+
+
+if __name__ == "__main__":
+    main()
+    displayWelcome()
+    filing_status = getFilingStatus()
+    gross_income = getGrossIncome()
+
+    if (filing_status == 1):
+        fed_cap = constants.fed_single_caps
+        kansas_cap = constants.kansas_single_caps
+        filing_status = 'Single'
+    elif (filing_status == 2):
+        fed_cap = constants.fed_married_caps
+        kansas_cap = constants.kansas_married_caps
+        filing_status = 'Married'
+    elif (filing_status == 3):
+        fed_cap = constants.fed_hoh_caps
+        kansas_cap = constants.kansas_hoh_caps
+        filing_status = 'Head of Household'
 
 # --- Federal Tax Calculation --- #
 fed_taxes_paid = 0
-left_to_tax = income
+left_to_tax = gross_income
 
 for x in range(len(fed_cap)):
     if fed_cap[x] < left_to_tax:
@@ -66,7 +82,7 @@ for x in range(len(fed_cap)):
 
 # --- State Tax Calculation --- #
 kansas_taxes_paid = 0
-left_to_tax = income
+left_to_tax = gross_income
 
 for y in range(len(kansas_cap)):
     if kansas_cap[y] < left_to_tax:
@@ -79,23 +95,23 @@ for y in range(len(kansas_cap)):
         left_to_tax = 0
 
 # --- FICA Calculations --- #
-social_security_paid = income * constants.social_security_rate
+social_security_paid = gross_income * constants.social_security_rate
 
 if social_security_paid >= constants.social_security_max:
     social_security_paid = constants.social_security_max
 
-if income > constants.medicare_premium_wage:
+if gross_income > constants.medicare_premium_wage:
     medicare_paid = round(
-        income * (constants.medicare_rate + constants.medicare_premium_rate), 2)
+        gross_income * (constants.medicare_rate + constants.medicare_premium_rate), 2)
 else:
-    medicare_paid = round(income * constants.medicare_rate, 2)
+    medicare_paid = round(gross_income * constants.medicare_rate, 2)
 
 # --- Final Calculations --- #
 fed_taxes_paid = round(fed_taxes_paid, 2)
 kansas_taxes_paid = round(kansas_taxes_paid, 2)
 total_taxes_paid = round((fed_taxes_paid + kansas_taxes_paid), 2)
 income_after_tax = round(
-    (income - total_taxes_paid - medicare_paid - social_security_paid), 2)
+    (gross_income - total_taxes_paid - medicare_paid - social_security_paid), 2)
 monthly_income = round((income_after_tax / constants.months_in_year), 2)
 weekly_income = round((income_after_tax / constants.weeks_in_year), 2)
 
